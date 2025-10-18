1aab98377dd7f2d7e7dc652141b932daec101bca
ReemAlshehri04 <re2malshehri@gmail.com>
2025-10-18 17:48:17 +0000

    done

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,11 +1,4 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int n);
-int print_last_digit(int n);
-void jack_bauer(void);
-void times_table(void);
+int add(int a, int b);
 #endif
