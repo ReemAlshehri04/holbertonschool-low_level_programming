e2b575acd9a72ac073accecfcaf8da9360f1622e
ReemAlshehri04 <re2malshehri@gmail.com>
2025-10-26 13:38:28 +0000

    done

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+	* _strcat - concatenates two strings
+	* @dest: destination string
+	* @src: source string
+	*
+	* Return: pointer to the resulting string dest
+	*/
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+int j = 0;
+/* Find the end of dest */
+while (dest[i] != '\0')
+i++;
+/* Copy src to dest starting from the end of dest */
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+/* Add terminating null byte */
+dest[i] = '\0';
+return (dest);
+}
