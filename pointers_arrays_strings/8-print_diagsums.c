d5bb454772c3df8f70996faa09627ec3b79da821
ReemAlshehri04 <re2malshehri@gmail.com>
2025-10-29 13:24:19 +0000

    done

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -14,8 +14,9 @@ int i;
 int sum1 = 0, sum2 = 0;
 for (i = 0; i < size; i++)
 {
-sum1 += a[i * size + i];             /* diagonal from top-left to bottom-right */
-sum2 += a[i * size + (size - 1 - i)]; /* diagonal from top-right to bottom-left */
+sum1 += a[i * size + i]; /* diagonal from top-left to bottom-right */
+sum2 += a[i * size + (size - 1 - i)];
+/* diagonal from top-right to bottom-left */
 }
 printf("%d, %d\n", sum1, sum2);
 }
