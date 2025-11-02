3e1abfde9d70692739846eca69624289ba798522
ReemAlshehri04 <re2malshehri@gmail.com>
2025-11-02 14:12:55 +0000

    done

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -7,10 +7,10 @@
  * Return: result of x^y, or -1 if y is negative
  */
 int _pow_recursion(int x, int y)
-{ 
+{
 if (y < 0)
-return (-1);            
+return (-1);
 if (y == 0)
-return (1);            
+return (1);
 return (x * _pow_recursion(x, y - 1));
 }
