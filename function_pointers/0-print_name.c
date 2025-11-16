00948a3d2b53b312048d2092f9a24403672031ff
ReemAlshehri04 <re2malshehri@gmail.com>
2025-11-16 12:10:35 +0000

    done

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+#include "function_pointers.h"
+/**
+ * print_name - calls a function to print a name
+ * @name: the name to print
+ * @f: pointer to a function that takes a char * and returns nothing
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+return;
+f(name);
+}
