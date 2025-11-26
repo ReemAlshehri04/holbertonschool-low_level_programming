3b46272a47d307c60214eeead28ea8e71c5c7728
ReemAlshehri04 <re2malshehri@gmail.com>
2025-11-26 12:04:15 +0000

    done

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -17,5 +17,12 @@ char *str;
 unsigned int len;
 struct list_s *next;
 } list_t;
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: number of nodes in the list
+ */
+size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
 #endif
