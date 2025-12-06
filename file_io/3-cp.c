dff87ae50908990184e84980bfe38140a87d4f30
ReemAlshehri04 <re2malshehri@gmail.com>
2025-12-06 17:19:05 +0000

    done

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,71 +1,94 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#define BUFFER_SIZE 1024
 /**
- * close_fd - closes a file descriptor with error checking
- * @fd: file descriptor
- */
-void close_fd(int fd)
-{
-if (close(fd) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-exit(100);
-}
-}
-/**
- * main - copies content of one file to another
- * @argc: argument count
- * @argv: argument vector
+ * 3-cp.c
  *
- * Return: 0 on success, exits with codes on failure
+ * This program copies the content of one file into another.
+ *
+ * Features:
+ * - Takes two arguments: file_from and file_to.
+ * - Reads data from file_from in chunks of 1024 bytes.
+ * - Writes the data into file_to (creates/truncates as needed).
+ *
+ * Error Handling:
+ * - Exit 97: wrong argument count → prints "Usage: cp file_from file_to".
+ * - Exit 98: cannot read from file_from.
+ * - Exit 99: cannot write to file_to.
+ * - Exit 100: cannot close a file descriptor.
+ *
+ * Permissions:
+ * - Created file_to has permissions rw-rw-r-- (0664).
+ *
+ * Notes:
+ * - Uses dprintf to print error messages to STDERR.
+ * - Uses open(), read(), write(), close() system calls.
+ * - Ensures proper cleanup on error (closing file descriptors when needed).
  */
-int main(int argc, char *argv[])
+void check_args(int ac)
+{
+if (ac != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+}
+void check_read(ssize_t r, char *file, int fd)
+{
+if (r == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+if (fd != -1)
+close(fd);
+exit(98);
+}
+}
+void check_write(ssize_t w, char *file, int fd_from, int fd_to)
+{
+if (w == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+if (fd_from != -1)
+close(fd_from);
+if (fd_to != -1)
+close(fd_to);
+exit(99);
+}
+}
+int main(int ac, char **av)
 {
 int fd_from, fd_to;
 ssize_t r, w;
-char buffer[BUFFER_SIZE];
-if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-exit(97);
-}
-/* Open source file */
-fd_from = open(argv[1], O_RDONLY);
+char buffer[1024];
+check_args(ac);
+fd_from = open(av[1], O_RDONLY);
 if (fd_from == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 exit(98);
 }
-/* Open destination file */
-fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (fd_to == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close_fd(fd_from);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+close(fd_from);
 exit(99);
 }
-/* Read 1024 bytes at a time */
-while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+while ((r = read(fd_from, buffer, 1024)) > 0)
 {
 w = write(fd_to, buffer, r);
-if (w == -1 || w != r)
+check_write(w, av[2], fd_from, fd_to);
+}
+check_read(r, av[1], fd_from);
+if (close(fd_from) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close_fd(fd_from);
-close_fd(fd_to);
-exit(99);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+exit(100);
 }
-}
-if (r == -1)
+if (close(fd_to) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-close_fd(fd_from);
-close_fd(fd_to);
-exit(98);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+exit(100);
 }
-close_fd(fd_from);
-close_fd(fd_to);
 return (0);
 }
