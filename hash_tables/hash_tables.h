80b6fab85c444ec4a8c5de39b38d0d7c15dac07b
ReemAlshehri04 <re2malshehri@gmail.com>
2025-12-13 01:25:28 +0000

    done

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -32,4 +32,5 @@ hash_node_t **array;
  * Return: Pointer to the newly created hash table, or NULL on failure
  */
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 #endif
