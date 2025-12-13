1d6e2b820e488ef1875cd164a8cdc5a0bb3b1732
ReemAlshehri04 <re2malshehri@gmail.com>
2025-12-13 02:30:50 +0000

    done

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,3 +1,9 @@
+/**
+ * hash_djb2 - Hash function implementing the djb2 algorithm
+ * @str: String to hash
+ *
+ * Return: Hash value
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 unsigned long int hash;
